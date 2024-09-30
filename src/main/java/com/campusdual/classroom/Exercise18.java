e333cc02efa2cb94bbca5b0840c594c2a2ee5208
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-09-30 21:15:52 +0200

    Ejercicio 18 resuelto

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,38 @@
 package com.campusdual.classroom;
 
+import static com.campusdual.util.Utils.integer;
+
 public class Exercise18 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        int arrySize = integer("Introduce un numero ");
 
-	}
+        int[] intArray = createAndInitializeArray(arrySize);
+
+        showInlineArray(intArray);
+    }
+
+    public static int[] createAndInitializeArray(int num) {
+        int[] intArray = new int[num];
+        for (int i = 0; i < num; i++) {
+            intArray[i] = i + 1;
+        }
+        return intArray;
+    }
+
+    public static void showInlineArray(int[] intArray) {
+//		StringJoiner joiner = new StringJoiner(" ");
+////		for (int num: intArray) {
+////			joiner.add(String.valueOf(num));
+////		}
+//		System.out.print(joiner);
+        StringBuilder builder = new StringBuilder();
+        for (int num : intArray) {
+            builder.append(num);
+            builder.append(" ");
+        }
+        builder.delete(builder.length() - 1, builder.length());
+        System.out.println(builder);
+    }
 }
+
